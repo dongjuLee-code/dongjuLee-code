003d6061c94231398abd2749fa642abf9c25e642
Lee_Coder <80030418+dongjuLee-code@users.noreply.github.com>
2022-06-07 18:35:47 +0900

    edit

--- examonline/apps/operation/__init__.py
+++
@@ -1,2 +0,0 @@
-# admin에서 app 이름 바꾸기
-default_app_config = 'operation.apps.OperationConfig'
--- examonline/apps/testpaper/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class TestpaperConfig(AppConfig):
-    name = 'testpaper'
-    # admin에서 app 이름 바꾸기
-    verbose_name = '시험지 정보（TP_Info）'
